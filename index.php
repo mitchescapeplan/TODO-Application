e6c5eff5a691f2425834348bff85158efae9a6f9
Mitchell Conte <mitchconte@icloud.com>
2018-01-10 23:47:51 -0500

    Basic table formatting and creation of other pages to show tasks in db

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	</div>
 	<div id="container-body">
 		<h2>Total Tasks:</h2>  <p>Number</p>
-		<h3>Pending:</h3>   <p>Number</p>
+		<a href="pending.php"><h3>Pending:</h3></a>   <p>Number</p>
 		<h3>Started:</h3>	<p>Number</p>
 		<h3>Completed:</h3>		<p>Number</p>
 		<h3>Late:</h3>		<p>Number</p>
---
+++ pending.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TODO List</title>
+<link rel="stylesheet" href="css.css">
+</head>
+<body>
+	<div id="container-header">
+		<h1>Pending Tasks</h1>
+	</div>
+	<div id="container-body">
+		<table>
+			<tr>
+				<th>Task Name</th>
+				<th>Task Description</th>
+				<th>Completion Date</th>
+				<th>Status</th>
+			<tr>
+			<tr>
+				<td>name</td>
+				<td>The task that I am trying to do is one of upmost importance. I need to make sure I take the car today</td>
+				<td>date</td>
+				<td>pending</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
