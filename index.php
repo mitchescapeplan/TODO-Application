23b966698d08ac4baf9f72dd0ac14f7f113ae940
Mitchell Conte <mitchconte@icloud.com>
2018-01-10 22:24:15 -0500

    Homepage with some CSS that shows dummy numbers now

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 <title>TODO List</title>
+<link rel="stylesheet" href="css.css">
 </head>
 <body>
-Does this work?
+	<div id="container-header">
+		<h1>Todo List</h1>
+	</div>
+	<div id="container-body">
+		<h2>Total Tasks:</h2>  <p>Number</p>
+		<h3>Pending:</h3>   <p>Number</p>
+		<h3>Started:</h3>	<p>Number</p>
+		<h3>Completed:</h3>		<p>Number</p>
+		<h3>Late:</h3>		<p>Number</p>
+	</div>
 </body>
 </html>
