39de6f1519567705affe57af5c135b76b31d2d5d
Mitchell Conte <mitchconte@icloud.com>
2018-01-13 16:14:46 -0500

    Displays correct numbers on home page

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
-	$dbconnection = new mysqli("localhost", "root", "", "todo") or die("Cannot connect");
-	@$total = $dbconnection->query("SELECT COUNT(*) FROM 'tasks'");
+	$dbconnection = mysqli_connect('localhost', 'root', '', 'todo');
+	
+	$queryresult = $dbconnection->query('SELECT COUNT(*) FROM tasks');
+	$total = $queryresult->fetch_array(MYSQLI_NUM);
+	
+	$pendingquery = $dbconnection->query("SELECT COUNT(*) FROM tasks WHERE Status = 'pending'");
+	$pending = $pendingquery->fetch_array(MYSQLI_NUM);
+	
+	$startedquery = $dbconnection->query("SELECT COUNT(*) FROM tasks WHERE Status = 'started'");
+	$started = $startedquery->fetch_array(MYSQLI_NUM);
+	
+	$completedquery = $dbconnection->query("SELECT COUNT(*) FROM tasks WHERE Status = 'completed'");
+	$completed = $completedquery->fetch_array(MYSQLI_NUM);
+	
+	$latequery = $dbconnection->query("SELECT COUNT(*) FROM tasks WHERE Status = 'late'");
+	$late = $latequery->fetch_array(MYSQLI_NUM);
 ?>
 
 <!DOCTYPE html>
@@ -14,11 +28,11 @@
 		<h1>Todo List</h1>
 	</div>
 	<div id="container-body">
-		<a href="tasks.php"><h2>Total Tasks:</h2></a>  <p></p>
-		<a href="pending.php"><h3>Pending:</h3></a> <?php echo " The number is $total" ?>  <p>Number</p>
-		<a href="started.php"><h3>Started:</h3></a>	<p>Number</p>
-		<a href="completed.php"><h3>Completed:</h3></a>	<p>Number</p>
-		<a href="late.php"><h3>Late:</h3></a> <p>Number</p>
+		<a href="tasks.php"><h2>Total Tasks:</h2></a> <p><?php echo $total[0]?></p>
+		<a href="pending.php"><h3>Pending:</h3></a> <p><?php echo $pending[0]?></p>
+		<a href="started.php"><h3>Started:</h3></a>	<p><?php echo $started[0]?></p>
+		<a href="completed.php"><h3>Completed:</h3></a>	<p><?php echo $completed[0]?></p>
+		<a href="late.php"><h3>Late:</h3></a> <p><?php echo $late[0]?></p>
 	</div>
 </body>
 </html>
